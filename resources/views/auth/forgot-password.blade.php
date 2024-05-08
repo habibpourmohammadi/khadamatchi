11c22116991fde941dc8a56ad71a4a1e68e87444
Habib <habibpourmohammady@gmail.com>
2024-05-08 22:09:07 +0330

    The button component has been added to sections related to authentication.

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -36,10 +36,7 @@
                         <small class="text-red-600 ms-1 font-bold">{{ $message }}</small>
                     @enderror
                 </div>
-                <button type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    ارسال ایمیل بازنشانی
-                </button>
+                <x-inputs.button type="submit" name="ارسال ایمیل بازنشانی" />
             </form>
         </div>
     </main>
